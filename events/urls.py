5f348bcc18b1cd2564db1795f32219e670542c58
paulokam <paulo.kammradt@gmail.com>
2022-05-24 02:49:29 -0300

    Reupload

--- events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import EventListView
-
-app_name = "events"
-urlpatterns = [
-    path('minha-historia-pessoal/', EventListView.as_view(), name='personal'),
-]
--- events/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import DetailView, ListView
-from .models import Event
-
-class EventListView(ListView):
-    model = Event
-
-class EventDetailView(DetailView):
-    model = Event
